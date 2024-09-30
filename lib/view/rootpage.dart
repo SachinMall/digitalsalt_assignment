abb6b20fea3f969978efc2a1351375865608c9f0
SachinMall <sachinvinodmall@gmail.com>
2024-09-30 13:12:16 +0530

    fixes

--- lib/view/rootpage.dart
+++ lib/view/rootpage.dart
@@ -54,103 +54,120 @@ class _RootPageState extends State<RootPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: PageTransitionSwitcher(
-          transitionBuilder: (child, primaryAnimation, secondaryAnimation) =>
-              FadeThroughTransition(
-            animation: primaryAnimation,
-            secondaryAnimation: secondaryAnimation,
-            child: child,
-          ),
-          child: pageOptions[_selectedIndex == 2 ? 0 : _selectedIndex],
-        ),
-        floatingActionButton: FloatingActionButton(
-          
-          onPressed: () {
-            Get.to(() => const SearchPage());
-          },
-           backgroundColor: AppColor.klightGrey.withOpacity(.5),
-          elevation: 0,
-          shape: const CircleBorder(
-              side: BorderSide(color: AppColor.kwhite, width: 4)),
-          child: SvgPicture.asset(IconAssets.searchActiveIcon,height: 20,width: 20,fit: BoxFit.fill,),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        bottomNavigationBar: Theme(
-          data: ThemeData(
-              splashColor: Colors.transparent,
-              highlightColor: Colors.transparent),
-          child: Container(
-            height: 75,
-            decoration: BoxDecoration(
-              color: Colors.transparent,
-              boxShadow: [
-                BoxShadow(
-                  color: AppColor.kblack.withOpacity(0.2),
-                  spreadRadius: 2,
-                  blurRadius: 10,
-                  offset: const Offset(0, 4),
-                ),
-              ],
+  return Scaffold(
+    body: PageTransitionSwitcher(
+      transitionBuilder: (child, primaryAnimation, secondaryAnimation) =>
+          FadeThroughTransition(
+        animation: primaryAnimation,
+        secondaryAnimation: secondaryAnimation,
+        child: child,
+      ),
+      child: pageOptions[_selectedIndex],
+    ),
+    bottomNavigationBar: Theme(
+      data: ThemeData(
+        splashColor: Colors.transparent,
+        highlightColor: Colors.transparent,
+      ),
+      child: Container(
+        height: 75,
+        decoration: BoxDecoration(
+          color: Colors.transparent,
+          boxShadow: [
+            BoxShadow(
+              color: AppColor.kblack.withOpacity(0.2),
+              spreadRadius: 2,
+              blurRadius: 10,
+              offset: const Offset(0, 4),
             ),
-            child: ClipRRect(
-              borderRadius: const BorderRadius.only(
-                topLeft: Radius.circular(20),
-                topRight: Radius.circular(20),
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: const BorderRadius.only(
+            topLeft: Radius.circular(20),
+            topRight: Radius.circular(20),
+          ),
+          child: BottomNavigationBar(
+            backgroundColor: AppColor.kwhite,
+            elevation: 0,
+            
+            selectedLabelStyle: const TextStyle(
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+              color: AppColor.kprimaryColor,
+            ),
+            unselectedLabelStyle: const TextStyle(
+              color: AppColor.klightGrey,
+              fontSize: 12,
+              fontWeight: FontWeight.w500,
+            ),
+            unselectedFontSize: 12,
+            showUnselectedLabels: true,
+            type: BottomNavigationBarType.fixed,
+            items: [
+              BottomNavigationBarItem(
+                activeIcon: SvgPicture.asset(IconAssets.homeActiveIcon),
+                icon: SvgPicture.asset(IconAssets.homeInactiveIcon),
+                label: 'Home',
               ),
-              child: BottomNavigationBar(
-                backgroundColor: AppColor.kwhite,
-                
-                elevation: 0,
-                selectedLabelStyle: const TextStyle(
-                    fontSize: 12,
-                    fontWeight: FontWeight.w500,
+              BottomNavigationBarItem(
+                activeIcon: SvgPicture.asset(IconAssets.courseActiveIcon),
+                icon: SvgPicture.asset(IconAssets.courseInactiveIcon),
+                label: 'Course',
+              ),
+              const BottomNavigationBarItem(
+                icon: SizedBox.shrink(),
+                label: '',
+              ),
+              BottomNavigationBarItem(
+                activeIcon: SvgPicture.asset(IconAssets.messageInactiveIcon,
                     color: AppColor.kprimaryColor),
-                unselectedLabelStyle: const TextStyle(
-                  color: AppColor.klightGrey,
-                  fontSize: 12,
-                  fontWeight: FontWeight.w500,
-                ),
-                unselectedFontSize: 12,
-                showUnselectedLabels: true,
-                
-                type: BottomNavigationBarType.fixed,
-                items: [
-                  BottomNavigationBarItem(
-                    activeIcon: SvgPicture.asset(IconAssets.homeInactiveIcon),
-                    icon: SvgPicture.asset(IconAssets.homeActiveIcon),
-                    label: 'Home',
-                  ),
-                  BottomNavigationBarItem(
-                    activeIcon: SvgPicture.asset(IconAssets.courseActiveIcon),
-                    icon: SvgPicture.asset(IconAssets.courseInactiveIcon),
-                    label: 'Course',
-                  ),
-                      
-                  BottomNavigationBarItem(
-                    activeIcon:
-                        // ignore: deprecated_member_use
-                        SvgPicture.asset(IconAssets.messageInactiveIcon,   color: AppColor.kprimaryColor),
-                    // ignore: deprecated_member_use
-                    icon: SvgPicture.asset(IconAssets.messageInactiveIcon
-                       ),
-                    label: 'Message',
-                  ),
-                  BottomNavigationBarItem(
-                    activeIcon:
-                        // ignore: deprecated_member_use
-                        SvgPicture.asset(IconAssets.accountInactiveIcon,   color: AppColor.kprimaryColor),
-                    // ignore: deprecated_member_use
-                    icon: SvgPicture.asset(IconAssets.accountInactiveIcon
-                        ),
-                    label: 'Account',
-                  ),
-                ],
-                currentIndex: _selectedIndex,
-                onTap: _onItemTapped,
+                icon: SvgPicture.asset(IconAssets.messageInactiveIcon),
+                label: 'Message',
+              ),
+              BottomNavigationBarItem(
+                activeIcon: SvgPicture.asset(IconAssets.accountInactiveIcon,
+                    color: AppColor.kprimaryColor),
+                icon: SvgPicture.asset(IconAssets.accountInactiveIcon),
+                label: 'Account',
+              ),
+            ],
+            currentIndex: _selectedIndex,
+            onTap: _onItemTapped,
+          ),
+        ),
+      ),
+    ),
+    floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    floatingActionButton: Stack(
+      alignment: Alignment.bottomCenter,
+      children: [
+        Positioned(
+         bottom: 25,
+          child: SizedBox(
+            height: 70,
+            width: 70,
+            child: FloatingActionButton(
+               
+              onPressed: () {
+                Get.to(() => const SearchPage());
+              },
+              backgroundColor: const Color(0xfff5f7ff),
+              elevation: 0,
+              shape: const CircleBorder(
+                side: BorderSide(color: AppColor.kwhite, width: 6),
+              ),
+              child: SvgPicture.asset(
+                IconAssets.searchActiveIcon,
+                height: 20,
+                width: 20,
+                fit: BoxFit.fill,
               ),
             ),
           ),
-        ));
-  }
+        ),
+      ],
+    ),
+  );
+}
 }
